7de922c00fb4647724e4114e880b9e59c63f0060
gopi374 <an3201088@gmail.com>
2025-03-27 19:08:57 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: 'https://gopi374.github.io/karan-mehndi-art/', // 👈 Set your repo name here
+  base: "./",
+  build: {
+    outDir: "dist",
+  },
 });
